42b7753c539dff43a469f4df32744f6cc4f75201
kt0d <kt0d@proton.me>
2019-12-03 21:05:22 +0100

    Add forward delcarations of functions

--- main.cpp
+++ main.cpp
@@ -17,6 +17,16 @@
 #include <libgen.h>
 #include <argp.h>
 
+struct comparison_result
+{
+	int diff;
+	int small;
+	int inf;
+};
+
+
+// Global variables 
+
 const std::string csv_header = "graph;vertices;edges;source;kernel;time;depth";
 const std::string csv_header_correct = "graph;vertices;edges;source;kernel;time;depth;correct";
 
@@ -31,9 +41,8 @@ struct arguments
 };
 
 static char args_doc[] = "GRAPH";
-static char doc[] = "  gpu-bfs - an implementation of breadth-first search using CUDA\v"
-"Program prints to stdout information in CSV format about every BFS run.\n";
-//"Please be aware of GPU memory limitations. Too many BFS runs in one program call can produce out of memory errors, depending on the size of a graph.";
+static char doc[] = "\tgpu-bfs - an implementation of breadth-first search using CUDA\v"
+"Program writes a CSV-formatted information about every breadth first search to standard output.";
 
 enum 
 {
@@ -43,102 +52,119 @@ enum
 static struct argp_option options[] =
 {
 	{args_doc,		0, 0, OPTION_DOC,
-		"An adjacency matrix of a graph, stored in Rutherford-Boeing compressed column format\n", file_group},
+		"An adjacency matrix of a graph, stored in Rutherford-Boeing "
+		"compressed column format\n", file_group},
 	{"print",		'p', 0,	0,
 		"Print size of a graph",print_group},
-
 	{"matrix",		'm', 0,	0,
 		"Print adjacency matrix of a graph",print_group},
 	{"source",		's', "VERTEX", 0,
-
 		"Select source vertex",setup_group},
-
 	{"times",		'n', "NUMBER", 0,
 		"Run every search NUMBER times", setup_group},
-
 	{"cpu",			'c', 0, 0,
-		"Run sequential BFS algorithm on CPU and compare results with other search methods", setup_group},
-
+		"Run sequential BFS algorithm on CPU and compare "
+		"results with other search methods", setup_group},
 	{"linear",		'L', 0, 0,
 		"Run linear-work BFS with blocking queue", simple_group},
-
 	{"quadratic",		'Q', 0, 0,
 		"Run simple quadratic-work BFS", simple_group},
-
 	{"expand-contract",	'E', 0, 0,
 		"Run expand-contract BFS", advanced_group},
-	
 	{"contract-expand",	'C', 0, 0,
 		"Run contract-expand BFS", advanced_group},
-
 	{"two-phase",		'T', 0, 0,
 		"Run two-phase BFS", advanced_group},
 	{0, 0, 0, 0, 0, 0}
 };
 
-static error_t parse_opt(int key, char* arg, struct argp_state *state)
-{
-	arguments *args = (arguments*)state->input;
 
-	switch(key)
-	{
-		case 'p':
-			args->print_info=true;
-			break;
-		case 'm':
-			args->print_matrix=true;
-			break;
-		case 's':
-			args->set_source = true;
-			args->set_source_vertex = atoi(arg);
-			break;
-		case 'n':
-			args->times = atoi(arg);
-			break;
-		case 'L':
-			args->kernels_to_run.push_back(std::make_pair("Linear",run_linear_bfs));
-			break;
-		case 'Q':
-			args->kernels_to_run.push_back(std::make_pair("Quadratic", run_quadratic_bfs));
-			break;
-		case 'E':
-			args->kernels_to_run.push_back(std::make_pair("Expand-contract", run_expand_contract_bfs));
-			break;
-		case 'C':
-			args->kernels_to_run.push_back(std::make_pair("Contract-expand", run_contract_expand_bfs));
-			break;
-		case 'T':
-			args->kernels_to_run.push_back(std::make_pair("Two-phase",run_two_phase_bfs));
-			break;
-		case 'c':
-			args->compare = true;
-			break;
-		case ARGP_KEY_ARG:
-			if(state->arg_num >= 1)
-				argp_failure(state, EXIT_FAILURE, 0, "too many arguments");
-			args->graph_location = arg;
-			break;
-		case ARGP_KEY_END:
-			if(state->arg_num < 1)
-				argp_failure(state, EXIT_FAILURE, 0, "too few arguments");
-			break;
-		default:
-			return ARGP_ERR_UNKNOWN;
-	}
-	return 0;
-}
+// Forward declarations of functions
+comparison_result compare_distance(const int* dist1,const int* dist2,const int n);
+void print_csv(std::string name, int n, int m, int s, const std::string k, const bfs::result result, const int* dist_cpu);
+static error_t parse_opt(int key, char* arg, struct argp_state *state);
 
 static struct argp argp = { options, parse_opt, args_doc, doc, nullptr, nullptr, nullptr};
 
-
-struct comparison_result
+int main(int argc, char **argv)
 {
-	int diff;
-	int small;
-	int inf;
-};
+	// Process options.
+	arguments args;
+	argp_parse(&argp, argc, argv, 0, 0, &args);
 
-// Compare dist2 to dist1, counting different values, values in dist2 smaller than in dist1, infinity balues in dist2
+	// Load graph.
+	std::ifstream rb_file;
+	rb_file.open(args.graph_location);
+	if(!rb_file.good())
+	{
+		std::cerr << "Incorrect file." << std::endl;
+		argp_help(&argp, stdout, ARGP_HELP_USAGE, argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	csr::matrix graph= csr::load_matrix(rb_file);
+	rb_file.close();
+
+	// If source vertex was chosen explicitly, check if it's correct.
+	if(args.set_source && (args.set_source_vertex < 0 || args.set_source_vertex >= graph.n))
+	{
+		std::cerr << "Vertex " << args.set_source_vertex << " does not belong to loaded graph." << std::endl;
+		argp_help(&argp, stdout, ARGP_HELP_USAGE, argv[0]);
+		csr::dispose_matrix(graph);
+		exit(EXIT_FAILURE);
+	}
+
+	// Get name of the graph from filename.
+	std::string graph_name = basename(&args.graph_location[0]);
+	if(args.print_info)
+		std::cout << graph_name << " with " << graph.n << " vertices and " << graph.nnz << " edges" << std::endl;
+	if(args.print_matrix)
+		csr::print_matrix(graph,std::cout,true,false);
+	if(args.kernels_to_run.size() == 0)
+	{
+		csr::dispose_matrix(graph);
+		return EXIT_SUCCESS;
+	}
+
+	std::random_device generator;
+	std::uniform_int_distribution<int> distribution(0,graph.n);
+
+	if(args.compare)
+		std::cout << csv_header_correct << std::endl;
+	else
+		std::cout << csv_header << std::endl;
+
+	// Run kernels.
+	for(int i = 0; i < args.times; i++)
+	{
+		const int source = args.set_source ? args.set_source_vertex : distribution(generator);
+
+		bfs::result cpu_result;
+		cpu_result.distance = nullptr;
+		if(args.compare)
+		{
+			cpu_result = cpu_bfs(graph,source);
+			print_csv(graph_name, graph.n, graph.nnz, source, "CPU", cpu_result, cpu_result.distance);
+		}
+
+		for (auto& it: args.kernels_to_run)
+		{
+			auto bfs_func = it.second;
+			bfs::result result = bfs_func(graph,source);
+
+			print_csv(graph_name, graph.n, graph.nnz, source, it.first, result, cpu_result.distance);
+
+			delete[] result.distance;
+		}
+
+		if(args.compare)
+			delete[] cpu_result.distance;
+	}
+
+	csr::dispose_matrix(graph);
+	return EXIT_SUCCESS;
+}
+
+// Compare dist2 to dist1, counting different values, values in dist2 smaller than in dist1 and infinity values in dist2.
 comparison_result compare_distance(const int* dist1,const int* dist2,const int n)
 {
 	comparison_result ret = {0, 0, 0};
@@ -179,80 +205,55 @@ void print_csv(std::string name, int n, int m, int s, const std::string k, const
 	std::cout << output.str();
 }
 
-int main(int argc, char **argv)
+// Function used to parse program arguments.
+static error_t parse_opt(int key, char* arg, struct argp_state *state)
 {
-	// Process options.
-	arguments args;
-	argp_parse(&argp, argc, argv, 0, 0, &args);
+	arguments *args = (arguments*)state->input;
 
-	// Load graph
-	std::ifstream rb_file;
-	rb_file.open(args.graph_location);
-	if(!rb_file.good())
+	switch(key)
 	{
-		std::cerr << "Incorrect file." << std::endl;
-		argp_help(&argp, stdout, ARGP_HELP_USAGE, argv[0]);
-		exit(EXIT_FAILURE);
+		case 'p':
+			args->print_info=true;
+			break;
+		case 'm':
+			args->print_matrix=true;
+			break;
+		case 's':
+			args->set_source = true;
+			args->set_source_vertex = atoi(arg);
+			break;
+		case 'n':
+			args->times = atoi(arg);
+			break;
+		case 'L':
+			args->kernels_to_run.push_back(std::make_pair("Linear",run_linear_bfs));
+			break;
+		case 'Q':
+			args->kernels_to_run.push_back(std::make_pair("Quadratic", run_quadratic_bfs));
+			break;
+		case 'E':
+			args->kernels_to_run.push_back(std::make_pair("Expand-contract", run_expand_contract_bfs));
+			break;
+		case 'C':
+			args->kernels_to_run.push_back(std::make_pair("Contract-expand", run_contract_expand_bfs));
+			break;
+		case 'T':
+			args->kernels_to_run.push_back(std::make_pair("Two-phase",run_two_phase_bfs));
+			break;
+		case 'c':
+			args->compare = true;
+			break;
+		case ARGP_KEY_ARG:
+			if(state->arg_num >= 1)
+				argp_error(state,  "too many arguments");
+			args->graph_location = arg;
+			break;
+		case ARGP_KEY_END:
+			if(state->arg_num < 1)
+				argp_error(state,  "too few arguments");
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
 	}
-	csr::matrix graph= csr::load_matrix(rb_file);
-	rb_file.close();
-
-	// If source vertex was chosen explicitly, check if it's correct.
-	if(args.set_source && (args.set_source_vertex < 0 || args.set_source_vertex >= graph.n))
-	{
-		std::cerr << "Vertex " << args.set_source_vertex << " does not belong to loaded graph." << std::endl;
-		argp_help(&argp, stdout, ARGP_HELP_USAGE, argv[0]);
-		csr::dispose_matrix(graph);
-		exit(EXIT_FAILURE);
-	}
-
-	// Get filename
-	std::string graph_name = basename(&args.graph_location[0]);
-	if(args.print_info)
-		std::cout << graph_name << " with " << graph.n << " vertices and " << graph.nnz << " edges" << std::endl;
-	if(args.print_matrix)
-		csr::print_matrix(graph,std::cout,true,false);
-	if(args.kernels_to_run.size() == 0)
-	{
-		csr::dispose_matrix(graph);
-		return EXIT_SUCCESS;
-	}
-
-	std::random_device generator;
-	std::uniform_int_distribution<int> distribution(0,graph.n);
-
-	if(args.compare)
-		std::cout << csv_header_correct << std::endl;
-	else
-		std::cout << csv_header << std::endl;
-	// Run kernels
-	for(int i = 0; i < args.times; i++)
-	{
-		const int source = args.set_source ? args.set_source_vertex : distribution(generator);
-
-		bfs::result cpu_result;
-		cpu_result.distance = nullptr;
-		if(args.compare)
-		{
-			cpu_result = cpu_bfs(graph,source);
-			print_csv(graph_name, graph.n, graph.nnz, source, "CPU", cpu_result, cpu_result.distance);
-		}
-
-		for (auto& it: args.kernels_to_run)
-		{
-			auto bfs_func = it.second;
-			bfs::result result = bfs_func(graph,source);
-
-			print_csv(graph_name, graph.n, graph.nnz, source, it.first, result, cpu_result.distance);
-
-			delete[] result.distance;
-		}
-
-		if(args.compare)
-			delete[] cpu_result.distance;
-
-	}
-
-	csr::dispose_matrix(graph);
-	return EXIT_SUCCESS;
+	return 0;
 }
